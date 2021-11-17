3dfc4b45ee818d01f052748fd8be8bd07eb797d4
Christian de Mondegreen <93417944+demondegreen@users.noreply.github.com>
2021-11-16 20:54:15 -0600

    some edits and formatting
    by going through and setting the formatting, the pseudocode inherits the same easy to read formatting as your active code uses.

--- R1/D14 Higher Lower/main_pseudocode.py
+++ R1/D14 Higher Lower/main_pseudocode.py
@@ -1,31 +1,33 @@
-##higher-lower game
-#
-# pseudocode run through before writing the program
+#higher-lower game
 
+#import random
 #import art pieces
 #import the game data dictionary
 
-##game function
-# init score
-# init gameover flag
-#playloop here 
-#clear screen
-#print the logo
-# if score > 0 print You're right! Current score: {score}.
-# load in data if there is no value, otherwise move B to A 
-#print the A data string
-# Compare A: Drake, a Musician, from Canada.
-#print the "VS" art
-#print the B data string
-# Against B: Shawn Mendes, a Musician, from Canada.
-# print the input prompt
-# Who has more followers? Type 'A' or 'B': 
-# if the answer is wrong, set gameoverflag
-# else increment the score and continue
-# playloop ends
+##game function def begin
+    # init score
+    # init gameover flag
+    #playloop here 
+        #clear screen
+        #print the logo
+        # if score > 0 print You're right! Current score: {score}.
+        ## load in data to A and B. 
+        # if B is non-zero then A = B
+        # otherwise load a random entry into A and remove it from the dictionary
+        # load a random entry into B and remove it from the dictionary
+        #print the A data string
+        # Compare A: Drake, a Musician, from Canada.
+        #print the "VS" art
+        #print the B data string
+        # Against B: Shawn Mendes, a Musician, from Canada.
+        # print the input prompt
+        # Who has more followers? Type 'A' or 'B': 
+        # if the answer is wrong, set gameoverflag
+        # else increment score and continue
+    # playloop end
 
-# print the final message, everybody loses eventually there is no win condition.
-#Sorry, that's wrong. Final score: {score}
-## game function def ends
+    # print the final message, everybody loses eventually there is no win condition.
+    #Sorry, that's wrong. Final score: {score}
+## game function def end
 
 #call game()
