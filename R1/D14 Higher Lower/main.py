5b33e8cfb3259dbbd16a1d0fdc5deadc87a7be3d
demondegreen <christiandemondegreen@gmail.com>
2021-11-18 12:44:31 -0600

    linted, commented out replit lib and clear() func

--- R1/D14 Higher Lower/main.py
+++ R1/D14 Higher Lower/main.py
@@ -1,13 +1,14 @@
-#higher-lower game
+# higher-lower game
 
 import random
-from replit import clear
+# from replit import clear
 
-#import art pieces
+# import art pieces
 from art import logo, vs
 
-#import the game data dictionary
+# import the game data dictionary
 from game_data import data
+
 
 def get_answer(_a, _b):
     """
@@ -20,6 +21,7 @@ def get_answer(_a, _b):
     else:
         return "b"
 
+
 def get_entry():
     """
     pulls an entry permanently off the global data stack and returns that entry
@@ -29,11 +31,13 @@ def get_entry():
     del data[index]
     return entry
 
+
 def format_entry_string(_entry):
     name = _entry['name']
     description = _entry['description']
     country = _entry['country']
     return f"{name}, {description}, from {country}."
+
 
 def game():
     # init score
@@ -47,36 +51,36 @@ def game():
     choice_a['follower_count'] = 0
     choice_b['follower_count'] = 0
 
-    #playloop begin
-    while gameover is False: 
-        clear()
+    # playloop begin
+    while gameover is False:
+        # clear()
         print(logo)
-        ## load in data to A and B. 
+        # load in data to A and B.
         if score > 0:
-            print (f"You're right! Current score: {score}.")
-            choice_a = choice_b            
+            print(f"You're right! Current score: {score}.")
+            choice_a = choice_b
         else:
-        # otherwise load a random entry into A and remove it from the dictionary
+            # otherwise load a random entry into A and remove it from the dictionary
             choice_a = get_entry()
         # load a random entry into B and remove it from the dictionary
         choice_b = get_entry()
 
-        #print the A data string
+        # print the A data string
         print("Compare A: " + format_entry_string(choice_a))
 
-        #print the "VS" art
+        # print the "VS" art
         print(vs)
 
-        #print the B data string
+        # print the B data string
         print(f"Against B: " + format_entry_string(choice_b))
 
         # print the input prompt
         user_response = input("Who has more followers? Type 'A' or 'B': ").lower()
-        
-        ## calculate the answer, moved to a function to calc the answer
+
+        # calculate the answer, moved to a function to calc the answer
         answer = get_answer(choice_a, choice_b)
 
-        ## compare response to answer
+        # compare response to answer
         # if the response is wrong, set gameoverflag
         if user_response != answer:
             gameover = True
@@ -85,11 +89,13 @@ def game():
         # else increment score and continue
         else:
             score += 1
-            if len(data) == 0: # deck is empty, game ends
+            if len(data) == 0:  # deck is empty, game ends
                 gameover = True
             # print the rare winning message
             print(f"Wow, you cleared the deck.  You win. Final score: {score}")
     # playloop end
-## game function def end
+
+
+# game function def end
 
 game()
