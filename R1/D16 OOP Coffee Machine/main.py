245559e8f8de4e58ec631c4c67eda99f2cd8c06f
demondegreen <christiandemondegreen@gmail.com>
2021-11-26 15:45:38 -0600

    R1D16 my final version before watching solution

--- R1/D16 OOP Coffee Machine/main.py
+++ R1/D16 OOP Coffee Machine/main.py
@@ -1,3 +1,24 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+power_on = True
+coffee_machine = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
+while power_on:
+    user_input = input(f"What would you like? ({menu.get_items()})")
+    if user_input == "off":
+        print("Powering down.  Goodbye.")
+        power_on = False
+    elif user_input == "report":
+        coffee_machine.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(user_input)
+        if drink is not None:
+            if coffee_machine.is_resource_sufficient(drink):
+                if money_machine.make_payment(drink.cost):
+                    coffee_machine.make_coffee(drink)
+
